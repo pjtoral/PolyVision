c056ef1974682c50fe2e5282e4cdde3fe52d2dc9
Paul John toral <78915086+pjtoral@users.noreply.github.com>
2023-07-16 22:36:27 +0800

    Added measuring function to Main

--- UI/trial.py
+++ UI/trial.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QVBoxLayout
 import sys
-
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from PyQt5.QtCore import *
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -16,17 +18,16 @@ class MainWindow(QMainWindow):
         popup.exec_()
 
 class PopupWindow(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        self.setWindowTitle("Popup Window")
-
+     def __init__(self, parent=None):
+        super().__init__()
+        self.setWindowTitle("Folder Images")
+        #Creation of Things
         self.button = QPushButton("Close", self)
+        self.button.setGeometry(QtCore.QRect(0, 170, 100, 100))
         self.button.clicked.connect(self.close)
+        self.moderateLabel      =   QtWidgets.QLabel("hello",self)
+        self.resize(1000,800)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.button)
-        self.setLayout(layout)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
